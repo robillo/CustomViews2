6b6ea394077e47feea3a55b895236eb15c7b343b
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-07-16 00:46:53 +0530

    Basic Layout

---
+++ app/src/main/java/com/appbusters/robinkamboj/customviews/MainActivity.java
@@ -0,0 +1,43 @@
+package com.appbusters.robinkamboj.customviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.one:{
+
+                break;
+            }
+            case R.id.two:{
+
+                break;
+            }
+            case R.id.three:{
+
+                break;
+            }
+            case R.id.four:{
+
+                break;
+            }
+            case R.id.five:{
+
+                break;
+            }
+            case R.id.six:{
+
+                break;
+            }
+        }
+    }
+}
